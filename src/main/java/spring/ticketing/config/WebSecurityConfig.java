a83e4903b64c7e6bd10fd07c57e9ffa83b8db632
Alex Mera <alxmera@gmail.com>
2018-07-11 18:43:45 -0500

    Completar la configuración de seguridad

--- src/main/java/spring/ticketing/config/WebSecurityConfig.java
+++ src/main/java/spring/ticketing/config/WebSecurityConfig.java
@@ -1,10 +1,17 @@
 package spring.ticketing.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.RoleHierarchyVoter;
+import org.springframework.security.access.vote.RoleVoter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
@@ -12,8 +19,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     http
         .csrf().disable()
         .authorizeRequests()
+        .antMatchers("/api/users/**", "/api/clients/**").hasAuthority("COORDINATOR")
+        .antMatchers("/api/tickets/**").hasAuthority("OPERATOR")
         .anyRequest().authenticated()
         .and()
         .httpBasic();
+  }
+
+  @Bean
+  public RoleHierarchy roleHierarchy() {
+    RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+    hierarchy.setHierarchy("COORDINATOR > OPERATOR and OPERATOR > CLIENT");
+    return hierarchy;
+  }
+
+  @Bean
+  public RoleVoter roleVoter() {
+    return new RoleHierarchyVoter(roleHierarchy());
   }
 }
